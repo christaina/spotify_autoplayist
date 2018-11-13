7c7c5cec3639ee75edcb25b912a33c1d5ba195a8
christina.bogdan <christina.bogdan@climate.com>
2018-11-13 13:11:48 -0800

    put code into separate folder

--- src/make_graph.py
+++ src/make_graph.py
@@ -47,8 +47,8 @@ def make_graph(df, counts):
 
 
 if __name__=="__main__":
-    song_hist_path = 'song_hist/song_history.csv'
-    graph_save_path = 'graph_viz/graph.json'
+    song_hist_path = '../song_hist/song_history.csv'
+    graph_save_path = '../graph_viz/graph.json'
 
     graph_cnt_threshold = 1 
     song_grp_threshold = 60 * 10 
